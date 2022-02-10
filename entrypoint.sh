741b1fcd616e6669945fe7dba3205146b8171b17
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-02-10 02:34:21 -0800

    Get server to start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -ue
 
-[ -z "${HIDDEN_ADMIN}" ] || echo "${HIDDEN_ADMIN} \"99:z\"" > /opt/game/hidden/addons/sourcemod/configs/admins_simple.ini
+[ -z "${HIDDEN_ADMIN}" ] || echo "${HIDDEN_ADMIN} \"99:z\"" > hidden/addons/sourcemod/configs/admins_simple.ini
 
-[ -z "${HIDDEN_MOTD}" ] || echo "${HIDDEN_MOTD}" > /opt/game/hidden/motd.txt
+[ -z "${HIDDEN_MOTD}" ] || echo "${HIDDEN_MOTD}" > hidden/motd.txt
 
-/usr/bin/Xvfb :99 -screen 0 16x16x8 &
+WINEARCH="win32" wine winecfg
 
-# Call srcds_linux instead of srcds_run to avoid restart logic
-DISPLAY=:99 /usr/bin/wine /opt/game/srcds.exe \
+Xvfb :1 -screen 0 800x600x16 &
+
+sleep 1s
+
+DISPLAY=":1.0" wine start srcds.exe \
     -game hidden \
     -port "$HIDDEN_PORT" \
     -strictbindport \
-    -usercon \
+    -console \
     -tickrate 100 \
     +ip 0.0.0.0 \
     +map "$HIDDEN_MAP" \
@@ -20,3 +23,5 @@ DISPLAY=:99 /usr/bin/wine /opt/game/srcds.exe \
     +sv_password "$HIDDEN_PASSWORD" \
     +sm_auth_by_steam_group_group_id "$STEAM_GROUP_ID" \
     +sm_auth_by_steam_group_steam_key "$STEAM_API_KEY"
+
+wineserver --wait
