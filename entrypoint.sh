f34c59d284b356050d7710124b2668a8efe4ef1f
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-03-20 11:51:08 -0700

    Reduce tickrate to 66

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ wine start /wait srcds.exe \
     -port "$HIDDEN_PORT" \
     -strictbindport \
     -console \
-    -tickrate 100 \
+    -tickrate 66 \
     +ip 0.0.0.0 \
     +map "$HIDDEN_MAP" \
     +rcon_password "$RCON_PASSWORD" \
